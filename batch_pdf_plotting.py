c85159fa4e0d6ddf5d6a90cc8a1adb0f46db727a
Juan Enciso <jenciso1@sheffield.ac.uk>
2019-07-24 19:30:27 +0100

    Remove comment lines from batch plotting script

--- batch_pdf_plotting.py
+++ batch_pdf_plotting.py
@@ -16,15 +16,8 @@ from multi_page_plotting import multiple_pages
 # %%
 if __name__ == "__main__":
     if not sys.stdin.isatty():
-        # There's data in stdin, should procceed using a list of files
         list_specs = [x.split() for x in sys.stdin.readlines()]
         multiple_pages(param_list=list_specs, par_dist=300)
     else:
-        # There is not data in stdin, use first item in sys.argv as path
-        # containing the edf files
         path_to_edf = sys.argv[1]
         multiple_pages(param_path=path_to_edf, par_dist=300)
-
-
-# SOMEDAY: Extend this so more parameters of the plot can be modified when
-# calling the script
